585ae8acc76f7b2e067a60174382d9dd6bbfebf6
seba <sebastianaversa@gmail.com>
2014-06-14 14:30:45 -0300

    fijate papu

--- src/screen.c
+++ src/screen.c
@@ -87,105 +87,65 @@ void mostrar_int(int error){
 	switch ( error)
 	{
 		case 0:
-		{
 			imprimir("Divide Error Exception (#DE)", vd);
 			break;
-		}
 		case 1:
-		{
 			imprimir("Debug Exception (#DB)", vd);
 			break;
-		}
 		case 2:
-		{
 			imprimir("NMI Interrupt", vd);
 			break;
-		}
 		case 3:
-		{
 			imprimir("Breakpoint Exception (#BP)", vd);
 			break;
-		}
 		case 4:
-		{
 			imprimir("Overflow Exception (#OF)", vd);
 			break;
-		}
 		case 5:
-		{
 			imprimir("BOUND Range Exceeded Exception (#BR)", vd);
 			break;
-		}
 		case 6:
-		{
 			imprimir("Invalid Opcode Exception (#UD)", vd);
 			break;
-		}
 		case 7:
-		{
 			imprimir("Device Not Available Exception (#NM)", vd);
 			break;
-		}
 		case 8:
-		{
 			imprimir("Double Fault Exception (#DF)", vd);
 			break;
-		}
 		case 9:
-		{
 			imprimir("Coprocessor Segment Overrun", vd);
 			break;
-		}
 		case 10:
-		{
 			imprimir("Invalid TSS Exception (#TS)", vd);
 			break;
-		}
 		case 11:
-		{
 			imprimir("Segment Not Present (#NP)", vd);
 			break;
-		}
 		case 12:
-		{
 			imprimir("Stack Fault Exception (#SS)", vd);
 			break;
-		}
 		case 13:
-		{
 			imprimir("General Protection Exception (#GP)", vd);
 			break;
-		}
 		case 14:
-		{
 			imprimir("Page-Fault Exception (#PF)", vd);
 			break;
-		}
 		case 15:
-		{
 			imprimir("", vd);
 			break;
-		}
 		case 16:
-		{
 			imprimir("x87 FPU Floating-Point Error (#MF)", vd);
 			break;
-		}
 		case 17:
-		{
 			imprimir("Alignment Check Exception (#AC)", vd);
 			break;
-		}
 		case 18:
-		{
 			imprimir("Machine-Check Exception (#MC)", vd);
 			break;
-		}
 		case 19:
-		{
 			imprimir("SIMD Floating-Point Exception (#XM)", vd);
 			break;
-		}
 	}
 }
 
@@ -195,17 +155,8 @@ void mostrar_num(int teclado){
 	vd += 79;
 	switch ( teclado)
 	{
-		case 2:
-		{
-			imprimir("1", vd);
-		}
-		case 3:
-		{
-			imprimir("2", vd);
-		}
-		case 4:
-		{
-			imprimir("3", vd);
-		}
+		case 2:	imprimir("1", vd);
+		case 3: imprimir("2", vd);
+		case 4: imprimir("3", vd);
 	}
 }
